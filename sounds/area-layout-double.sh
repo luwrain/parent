e127f7f387799cad7f0cd5ccf22ccc152623137d
Michael Pozhidaev <msp@luwrain.org>
2026-05-20 16:26:54 +0700

    Fixing area layout sounds

--- sounds/area-layout-double.sh
+++ sounds/area-layout-double.sh
@@ -4,24 +4,24 @@
 # Des major
 
 INS1=108
-INS2=82
-NOTE1=61
+INS2=72
+NOTE1=49
 NOTE2=61
 VOLUME=100
 DUR=90
 
-./melody.sh $INS1 $VOLUME $NOTE1 $DUR 68 $DUR | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .melody-src.wav
-sox -D .melody-src.wav  -r 256000 -c 1 -b 32 .melody-pre.wav bass 5
-sox -D --norm=-0.1 .melody-pre.wav -c 2 .melody.wav reverb 50  fade t 0 2.5 2.5
+./melody.sh $INS1 $VOLUME $NOTE1 $DUR 68 $DUR | csvmidi - > melody.midi && timidity -a -C 8 -s 256000   -Ow melody.midi > /dev/null && mv melody.wav .melody-src.wav
+sox .melody-src.wav  -e float -c 1 .melody-pre.wav bass 5
+sox --norm=-0.1 .melody-pre.wav -c 2 .melody.wav reverb 50  fade t 0 2.5 2.5
 
-./melody.sh $INS2 120 $NOTE2 120 | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .high-src.wav
-sox -D .high-src.wav  -r 256000 -b 32 -c 1 -b 16 .high-pre.wav
-sox -D --norm=-5  .high-pre.wav -c 2 .high.wav reverb 70  fade t 0 1.5 1.5
+./melody.sh $INS2 120 $NOTE2 120 | csvmidi - > melody.midi && timidity -a -C 8 -s 256000 -Ow melody.midi > /dev/null && mv melody.wav .high-src.wav
+sox .high-src.wav  -e float -c 1 .high-pre.wav
+sox --norm=-5  .high-pre.wav -c 2 .high.wav reverb 70  fade t 0 1.5 1.5
 
-sox -D -n -r 256000 -b 32 -c 2 .harm.wav \
-    synth 10 sin %-20 sin %-17 sin %-13 gain -25  pad 0.10 \
+sox -n -r 256000 -e float -c 2 .harm.wav \
+    synth 10 sin %-20 sin %-16 sin %-13 gain -25  pad 0.10 \
     fade t 0.1 1.4 1.3 
 
-sox -D .melody.wav .high.wav .harm.wav -m .pre.wav
-sox -D .pre.wav area-layout-double.wav  pad 0 1
+sox .melody.wav .high.wav .harm.wav -m .pre.wav
+sox --norm=-0.1 .pre.wav area-layout-double.wav  pad 0 1
 rm -f .*.midi .*.wav
--- sounds/area-layout.sh
+++ sounds/area-layout.sh
@@ -3,25 +3,25 @@
 # Copyright 2026 Michael Pozhidaev <msp@luwrain.org>
 # Des major
 
-NOTE1=61
+NOTE1=73
 NOTE2=61
 INS1=108
-INS2=82
+INS2=72
 VOLUME=100
 DUR=50
 
-./melody.sh $INS1 $VOLUME $NOTE1 $DUR | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .melody-src.wav
-sox .melody-src.wav  -r 256000 -c 1 -b 32 .melody-pre.wav bass 5
-sox -D --norm=-1 .melody-pre.wav -c 2 .melody.wav reverb 65  fade t 0 1.5 1.5
+./melody.sh $INS1 $VOLUME $NOTE1 $DUR | csvmidi - > melody.midi && timidity -a -C 8 -s 256000 -Ow melody.midi > /dev/null && mv melody.wav .melody-src.wav
+sox .melody-src.wav  -e float -c 1 .melody-pre.wav bass 5
+sox --norm=-1 .melody-pre.wav -c 2 .melody.wav reverb 65  fade t 0 1.5 1.5
 
-./melody.sh $INS2 80 $NOTE2 50 | csvmidi - > melody.midi && timidity -Ow melody.midi > /dev/null && mv melody.wav .high-src.wav
-sox .high-src.wav  -r 256000 -c 1 -b 32 .high-pre.wav
-sox -D --norm=-5  .high-pre.wav -c 2 .high.wav reverb 70  fade t 0 1.5 1.5
+./melody.sh $INS2 80 $NOTE2 50 | csvmidi - > melody.midi && timidity -a -C 8 -s 256000 -Ow melody.midi > /dev/null && mv melody.wav .high-src.wav
+sox .high-src.wav  -e float -c 1 .high-pre.wav
+sox --norm=-5  .high-pre.wav -c 2 .high.wav reverb 70  fade t 0 1.5 1.5
 
-sox -D -n -r 256000 -b 32 -c 2 .harm.wav \
-    synth 10 sin %-20 sin %-17 sin %-13 gain -25  pad 0.10 \
+sox -n -r 256000 -e float -c 2 .harm.wav \
+    synth 10 sin %-20 sin %-16 sin %-13 gain -25  pad 0.10 \
     fade t 0.1 1.4 1.3 
 
-sox -D .melody.wav .high.wav  .harm.wav -m .pre.wav
-sox -D --norm=-1 .pre.wav area-layout.wav
+sox .melody.wav .high.wav  .harm.wav -m .pre.wav
+sox --norm=-1 .pre.wav area-layout.wav
 rm -f .*.midi .*.wav
